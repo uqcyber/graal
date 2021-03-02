874b6fbf7f7df51d34061ff8b3b7ff619e83200c
Leon <lkarstens5@gmail.com>
2021-03-03 06:18:11 +1000

    Added boolean to decide use as data flow or control flow.
    Added basic error reporting (load, store field nodes)

--- compiler/src/org.graalvm.compiler.core/src/org/graalvm/compiler/core/GraalInterpreter.java
+++ compiler/src/org.graalvm.compiler.core/src/org/graalvm/compiler/core/GraalInterpreter.java
@@ -29,11 +29,16 @@ import org.graalvm.compiler.nodes.calc.FloatingNode;
 import org.graalvm.compiler.nodes.java.LoadFieldNode;
 import org.graalvm.compiler.nodes.java.StoreFieldNode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 
+/*
+enum errorCodes {
+    LOAD_ERROR
+}
 // todo ensure we are working with the 1st graph! (initial "After Parsing" graph state)
 //  -> to avoid machine specific optimisations.
 
@@ -41,6 +46,28 @@ import java.util.Map;
 //todo look into extending phases?
 //todo look into GraalCompilerTest for some interesting test methods e.g. parseEager()
 
+//todo add a class for error output messages
+class ErrorReporter{
+    private static final Map<errorCodes, String> errorDict;
+
+    static{
+        errorDict =   new HashMap<errorCodes, String>() {{
+            put(errorCodes.LOAD_ERROR, "Loading from field with no assigned values");
+//            put(1, "Loading from field with no assigned values");
+        }};
+    }
+
+    public String getErrorCodes(ArrayList<Integer> errorCodes){
+        ArrayList<String> messages = new ArrayList<String>();
+        for(int elem: errorCodes) {
+            String errorMessage = errorDict.getOrDefault(elem, "");
+            messages.add(errorMessage);
+        }
+
+        return messages.toString();
+    }
+}
+ */
 
 /**
  * Evaluate all of the floating nodes / input nodes to values then supply these to the fixed nodes.
@@ -58,6 +85,7 @@ public class GraalInterpreter{ //implements NodeVisitor {
     private InterpreterFrame currentFrame;
     private Map<Integer, Object> state = new HashMap<>();
     private StructuredGraph currentGraph;
+    private ArrayList<String> errorMessages = new ArrayList<>();
 
     public Map<Integer, Object> getState(){
         return state;
@@ -69,37 +97,40 @@ public class GraalInterpreter{ //implements NodeVisitor {
         System.out.println("Now executing graph\n");
         currentFrame = new InterpreterFrame(0);
 
-        visit(graph.start());
+        visit(graph.start(), false);
 
-        System.out.printf("The return value was: %d", (int)state.get(-1));
-//        System.out.printf("The return value was: %d", 188);
+        if (state.containsKey(-2)) {
+            System.out.printf("The following issues were encountered when interpreting the graph: %s\n", state.get(-2));
+        } else {
+            System.out.printf("The return value was: %d", (int) state.get(-1));
+        }
     }
 
     // Global / Most generic visit pattern
     // todo could act as a sort of switch for visiting nodes (down casts based on runtime instance type)
-    public Object visit(Node node) {
+    public Object visit(Node node, boolean asInput) {
         if (node instanceof ValueNode){
-            return visit((ValueNode) node);
+            return visit((ValueNode) node, asInput);
         }
 
         System.err.println("Unhandled visit for: " + node.toString() + " of class " + node.getClass());
         return null;
     }
 
-    public Object visit(ValueNode node){
+    public Object visit(ValueNode node, boolean asInput){
 //        System.out.println("Of type ValueNode, deciding subclass: (Actually)" + node.getClass() + "\n");
 
         if (node instanceof FixedNode){
-            return visit((FixedNode) node);
+            return visit((FixedNode) node, asInput);
         }
         else if (node instanceof FloatingNode) {
-            return visit((FloatingNode) node);
+            return visit((FloatingNode) node, asInput);
         }
         System.err.println("Unhandled visit for: " + node.toString() + " of class " + node.getClass());
         return null;
     }
 
-    public Object visit(FloatingNode node){
+    public Object visit(FloatingNode node, boolean asInput){
 //        System.out.println("Of type FloatingNode, deciding subclass: (Actually)" + node.getClass() + "\n");
         if (node instanceof ConstantNode){
             return visit((ConstantNode) node);
@@ -108,30 +139,30 @@ public class GraalInterpreter{ //implements NodeVisitor {
         } else if (node instanceof ParameterNode){
             return visit((ParameterNode) node);
         } else if (node instanceof ValuePhiNode){
-            return visit((ValuePhiNode) node);
+            return visit((ValuePhiNode) node, asInput);
         }
 
         System.err.println("Unhandled visit for: " + node.toString() + " of class " + node.getClass());
         return null;
     }
 
-    public Object visit(FixedNode node) {
+    public Object visit(FixedNode node, boolean asInput) {
 //        System.out.println("Of type FixedNode, deciding subclass: (Actually)" + node.getClass() + "\n");
 
         if (node instanceof ControlSinkNode){
-            return visit((ControlSinkNode) node);
+            return visit((ControlSinkNode) node, asInput);
         } else if (node instanceof LoadFieldNode){
-            return visit((LoadFieldNode) node);
+            return visit((LoadFieldNode) node, asInput);
         } else if (node instanceof IfNode){
-            return visit((IfNode) node);
+            return visit((IfNode) node, asInput);
         } else if (node instanceof BeginNode){
-            return visit((BeginNode) node);
+            return visit((BeginNode) node, asInput);
         } else if (node instanceof EndNode){
-            return visit((EndNode) node);
+            return visit((EndNode) node, asInput);
         } else if (node instanceof MergeNode){
-            return visit((MergeNode) node);
+            return visit((MergeNode) node, asInput);
         } else if (node instanceof StoreFieldNode){
-            return visit((StoreFieldNode) node);
+            return visit((StoreFieldNode) node, asInput);
         }
 
         System.err.println("Unhandled visit for: " + node.toString() + " of class " + node.getClass());
@@ -141,69 +172,69 @@ public class GraalInterpreter{ //implements NodeVisitor {
     /**
      * Entry point for most graphs.
      */
-    public Object visit(StartNode node) {
+    public Object visit(StartNode node, boolean asInput) {
         System.out.println("Visiting " + node.getNodeClass().shortName() + "\n");
 
-        return visit(node.next());
+        return visit(node.next(), asInput);
     }
 
-    public Object visit(BeginNode node) {
+    public Object visit(BeginNode node, boolean asInput) {
         System.out.println("Visiting " + node.getNodeClass().shortName() + "\n");
 
-        return visit(node.next());
+        return visit(node.next(), asInput);
     }
 
-    public Object visit(EndNode node) {
+    public Object visit(EndNode node, boolean asInput) {
         System.out.println("Visiting " + node.getNodeClass().shortName() + "\n");
 
         //todo add case for multiple successors.
         for (Node mergeNode : node.cfgSuccessors()){
-            return visit(mergeNode); //todo currently just visits first cfgSuccessor
+            return visit(mergeNode, asInput); //todo currently just visits first cfgSuccessor
         }
 
         return null;
     }
 
-    public Object visit(MergeNode node){
+    public Object visit(MergeNode node, boolean asInput){
         System.out.println("Visiting " + node.getNodeClass().shortName() + "\n");
 
-        return visit(node.next());
+        return visit(node.next(), asInput);
     }
 
-    public Object visit(ValuePhiNode node){
+    public Object visit(ValuePhiNode node, boolean asInput){
         System.out.println("Visiting " + node.getNodeClass().shortName() + "\n");
 
-        return visit(node.firstValue());
+        return visit(node.firstValue(), asInput);
     }
 
-    public Object visit(LogicNode node){
+    public Object visit(LogicNode node, boolean asInput){
 
         System.out.println("Visiting " + node.getNodeClass().shortName() + "\n");
         //todo
         return true;
     }
 
-    public Object visit(IfNode node){
+    public Object visit(IfNode node, boolean asInput){
         System.out.println("Visiting " + node.getNodeClass().shortName() + "\n");
         LogicNode condition = node.condition();
         AbstractBeginNode trueSucc = node.trueSuccessor();
         AbstractBeginNode falseSucc = node.falseSuccessor();
 
-        boolean isTrue = (boolean) visit(condition);
+        boolean isTrue = (boolean) visit(condition, asInput);
 
         if (isTrue){
-            return visit(trueSucc);
+            return visit(trueSucc, asInput);
         }
 
-        return visit(falseSucc);
-    }
-
-    public Object visit(LoadFieldNode node){
-        System.out.println("Visiting/skipping " + node.getNodeClass().shortName());
-        return visit(node.next());
+        return visit(falseSucc, asInput);
     }
 
     public Object visit(LoadFieldNode node, boolean asInput){
+        System.out.println("Visiting " + node.getNodeClass().shortName());
+        if (!asInput) {
+            System.out.println("Skipping node");
+            return visit(node.next(), false);
+        }
         System.out.println("Getting value from " + node.getNodeClass().shortName());
 
         if (state.containsKey(node.field().getOffset())){
@@ -211,12 +242,15 @@ public class GraalInterpreter{ //implements NodeVisitor {
 //            System.out.printf(String.valueOf(state));
             return state.getOrDefault(node.field().getOffset(), 2);
         } else {
-            System.out.printf("No data stored in field - returning GARBAGE in %s\n\n", node.field().getOffset());
-            return 5;
+            System.out.printf("No data stored in field - returning GARBAGE from %s\n\n", node.field().getOffset());
+            state.put(-2, errorMessages);
+            errorMessages.add( String.format("Load from field without stored value (ID: %s, Field Offset: %s)", node.id(), node.field().getOffset()));
+            return 1;
         }
     }
 
-    public Object visit(StoreFieldNode node){
+
+    public Object visit(StoreFieldNode node, boolean asInput){
         System.out.println("Visiting " + node.getNodeClass().shortName());
 
         System.out.printf("Storing value: %s in %s - specifically in offset  %s\n", node.value(), node.field(), node.field().getOffset());
@@ -228,30 +262,35 @@ public class GraalInterpreter{ //implements NodeVisitor {
             System.out.printf("Storing CONSTANT value: %s\n",value);
         }
 
-        return visit(node.next());
+        return visit(node.next(), asInput);
     }
 
-    public Object visit(ControlSinkNode node){
+    public Object visit(ControlSinkNode node, boolean asInput){
         if (node instanceof ReturnNode){
-            return visit((ReturnNode) node);
+            return visit((ReturnNode) node, asInput);
         }
         return null;
     }
 
-    public Object visit(ReturnNode node) {
+    public Object visit(ReturnNode node, boolean asInput) {
         System.out.println("Visiting " + node.getNodeClass().shortName() + "\n");
         // todo using -1 as signifier for return -> should use unique node id.
-        state.put(-1, visit(node.result()));
+        state.put(-1, visit(node.result(), asInput));
         return null;
     }
+
+    // todo  consider other options to differentiate between retrieving a value (data flow) rather than control flow
+    // options: multiple return values
+    // alternate access method signatures. e.g. visit(LoadFieldNode, boolean asInput)
+    // Stack based?
 
     public Object visit(AddNode node){
         System.out.println("Visiting " + node.getNodeClass().shortName() + "\n");
 
         ValueNode nodeX = node.getX();
         ValueNode nodeY = node.getY();
-        Object x = visit((LoadFieldNode) nodeX, true); // todo deal with other input value nodes
-        Object y = visit((LoadFieldNode) nodeY, true);
+        Object x = visit(nodeX, true); // todo deal with other input value nodes
+        Object y = visit(nodeY, true);
 
         int sum = 0;
 
