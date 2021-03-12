cf46ae09107377c6eeefea6904c66233ebe705f2
Leon <lkarstens5@gmail.com>
2021-03-12 12:22:24 +1000

    Added LoadFieldNode and AddNode functionality to Visitor Methods

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/AddNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/AddNode.java
@@ -35,7 +35,9 @@ import org.graalvm.compiler.graph.spi.CanonicalizerTool;
 import org.graalvm.compiler.lir.gen.ArithmeticLIRGeneratorTool;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
 import org.graalvm.compiler.nodes.ConstantNode;
+import org.graalvm.compiler.nodes.NodalVisitor;
 import org.graalvm.compiler.nodes.NodeView;
+import org.graalvm.compiler.nodes.RuntimeType;
 import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.spi.NodeLIRBuilderTool;
 
@@ -204,5 +206,9 @@ public class AddNode extends BinaryArithmeticNode<Add> implements NarrowableArit
 
     protected boolean isExact() {
         return false;
+    }
+
+    public RuntimeType accept(NodalVisitor v){
+        return v.visit(this);
     }
 }
