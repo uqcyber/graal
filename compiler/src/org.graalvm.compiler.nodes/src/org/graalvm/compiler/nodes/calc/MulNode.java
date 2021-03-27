5601cdec4268a16813f04c7ff969efa6ced2800a
Leon <lkarstens5@gmail.com>
2021-03-27 18:35:30 +1000

    Added visitor to Sub and Mul nodes

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/MulNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/MulNode.java
@@ -37,7 +37,9 @@ import org.graalvm.compiler.graph.spi.CanonicalizerTool;
 import org.graalvm.compiler.lir.gen.ArithmeticLIRGeneratorTool;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
 import org.graalvm.compiler.nodes.ConstantNode;
+import org.graalvm.compiler.nodes.NodalVisitor;
 import org.graalvm.compiler.nodes.NodeView;
+import org.graalvm.compiler.nodes.RuntimeType;
 import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.spi.NodeLIRBuilderTool;
 
@@ -179,4 +181,6 @@ public class MulNode extends BinaryArithmeticNode<Mul> implements NarrowableArit
     protected boolean isExact() {
         return false;
     }
+
+    public RuntimeType accept(NodalVisitor v){ return v.visit(this); }
 }
--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/SubNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/SubNode.java
@@ -35,7 +35,9 @@ import org.graalvm.compiler.graph.spi.CanonicalizerTool;
 import org.graalvm.compiler.lir.gen.ArithmeticLIRGeneratorTool;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
 import org.graalvm.compiler.nodes.ConstantNode;
+import org.graalvm.compiler.nodes.NodalVisitor;
 import org.graalvm.compiler.nodes.NodeView;
+import org.graalvm.compiler.nodes.RuntimeType;
 import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.spi.NodeLIRBuilderTool;
 import org.graalvm.compiler.nodes.util.GraphUtil;
@@ -175,4 +177,6 @@ public class SubNode extends BinaryArithmeticNode<Sub> implements NarrowableArit
     protected boolean isExact() {
         return false;
     }
+
+    public RuntimeType accept(NodalVisitor v){ return v.visit(this); }
 }
