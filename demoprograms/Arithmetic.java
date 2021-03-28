ac8633c7c944aeadb5d4297a61cc0c0970e9fbbd
Leon <lkarstens5@gmail.com>
2021-03-28 17:13:49 +1000

    Updated Demo Test Cases
    (To be rewritten as unittests...)

---
+++ demoprograms/Arithmetic.java
@@ -0,0 +1,104 @@
+import java.util.Random;
+
+class Arithmetic{
+
+    static int a = 1;
+    static int b = 4;
+    static int c = 8;
+    static int d;
+
+    public static void main(String[] args) {
+        for (int i =0; i < 1000000; i++){
+            exec();
+        }
+    }
+
+    public static void exec(){
+        long s = 1235;
+
+        Random r = new Random();
+        r.setSeed(s);
+
+        //Subtraction
+        subFields();
+
+        //Addition
+        addConstants(); //5
+        addParams(r.nextInt(100), r.nextInt(100)); //74
+        addFields(); //5
+        addFields2(); //9
+        addFields3(); //26
+
+
+        averageConstants(); // 3
+        averageFields(); // 32
+        averageFields2(); // 
+        averageFields3(); // 1
+        averageParams(r.nextInt(100), r.nextInt(100)); // 45
+
+        // System.out.println(out);
+
+    }
+
+    public static int subFields(){
+        return a - b;
+    }
+
+    public static int addConstants(){
+        return 2 + 3;
+    }
+
+    // Add parameters
+    public static int addParams(int a, int b){
+        return a + b;
+    }
+
+    // Adds Static Fields a, b
+    public static int addFields(){
+        return a + b;
+    }
+
+    // Initialises and adds Static Fields c,d
+    public static int addFields2(){
+        c = 4;
+        d = 5;
+        return c + d;
+    }
+
+    // Uses the return value of an add in a following add operation
+    public static int addFields3(){
+        c = 12;
+        d = 2;
+        int e = c + d;
+        return e + c;
+    }
+
+    // Average (div nodes)
+    
+    public static int averageConstants() {
+        return (5 + 2) / 2;
+    }
+
+    // Is simplified to RighShift node
+    public static int averageFields() {
+        a = 5;
+        b = 60;
+        return (a + b) / 2;
+    }
+
+    // Is Simplified to signed div node
+    public static int averageFields2() {
+        a = 3;
+        b = 4;
+        c = 5;
+        return (a + b + c) / 3;
+    }
+
+    public static int averageFields3() {
+        return (a + b + c) / c;
+    }
+
+    public static int averageParams(int a, int b) {
+        return (a + b) / 2;
+    }
+}
--- demoprograms/ControlFlow.java
+++ demoprograms/ControlFlow.java
@@ -1,15 +1,15 @@
 import java.util.Random;
 
-class Addition{
+public class ControlFlow {
+
+    static int a = 1;
+    static int b = 4;
+    static int c;
+    static int d;
+
     public static void main(String[] args) {
         for (int i =0; i < 1000000; i++){
             loop();
-            adder(1,2);
-            average(6, 8);
-            basicAdd();
-            basicAdd2();
-            basicAdd3();
-            basicAdd4();
             branch(1,2);
             branch2();
             exec();
@@ -22,14 +22,6 @@ class Addition{
         return branch(r.nextInt(100), r.nextInt(100));
     }
 
-    public static int average(int a, int b) {
-        return (a + b) / 2;
-    }
-
-    public static int adder(int a, int b){
-        return a + b;
-    }
-
     public static void loop(){
         int a = 0;
         
@@ -38,17 +30,25 @@ class Addition{
         }
     }
 
-    static int a = 1;
-    static int b = 4;
-    static int c;
-    static int d;
-
     public static int branch(int a, int b){
 
         int out = 0;
         if (a < b){
             out = 1;
         }
+        return out;
+    }
+    public static int branch2(){
+        a = 1;
+        b = 2;
+        int out = 0;
+
+        if (a < b){
+            out = 1;
+        } else {
+            out = 2;
+        }
+
         return out;
     }
 
@@ -63,41 +63,4 @@ class Addition{
 
         return out;
     }
-
-    public static int branch2(){
-        a = 1;
-        b = 2;
-        int out = 0;
-
-        if (a < b){
-            out = 1;
-        } else {
-            out = 3;
-        }
-
-        return out;
-    }
-
-    
-    public static int basicAdd(){
-        return 2 + 3;
-    }
-
-    public static int basicAdd2(){
-        return a + b;
-    }
-
-    public static int basicAdd3(){
-        c = 4;
-        d = 5;
-        return c + d;
-    }
-
-    public static int basicAdd4(){
-        c = 12;
-        d = 2;
-        int e = c + d;
-        return e + c;
-    }
-    
 }
