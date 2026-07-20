949548560817c7466b92193a8b47022dd18ed536
default-jamc <default_jamc@outlook.com>
2026-07-20 15:56:48 +1000

    [IntegerBelowNode] Cleaning up a rewrite rule

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/calc/IntegerBelowNode.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/calc/IntegerBelowNode.java
@@ -189,12 +189,12 @@ public final class IntegerBelowNode extends IntegerLowerThanNode {
                      *
                      * x < n - C
                      */
-                    // veriopt: BelowLess: (x |<| ((n |<| c) ? const 0 : (n + c'))) |-> x < (n - c)
-                    //                      when (is_stamp_positive (stamp_expr x) & (wf_stamp x)              &&
-                    //                            is_stamp_positive (stamp_expr n) & (wf_stamp n)              &&
-                    //                            stamp_expr n = IntegerStamp b l h                            &&
-                    //                            c = const (new_int cb cv) & c' = const (new_int c'b c'v)     &&
-                    //                            c'v <s 0 & c'v == -cv
+                    // veriopt: BelowLess: (x |<| ((n |<| c) ? const (new_int b 0) : (n + c'))) |-> x < (n - c)
+                    //                      when (is_stamp_positive (stamp_expr x) && (wf_stamp x)        &&
+                    //                            is_stamp_positive (stamp_expr n) && (wf_stamp n)        &&
+                    //                            stp_bits (stamp_expr n) = b                             &&
+                    //                            c = const (new_int b cv) && c' = const (new_int b cvv)  &&
+                    //                            cvv <s 0 && cvv == -cv)
                     // TODO unsure
                     return IntegerLessThanNode.create(forX, SubNode.create(n, c, view), view);
                 }
